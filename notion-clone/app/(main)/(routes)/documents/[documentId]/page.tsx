70673f279d4be5a2a93cd042cde21dcee73ad0d2
Siyun <husiyun2018@gmail.com>
2024-02-02 22:44:47 -0500

    toolbar -> add icon

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,3 +1,39 @@
-export default function DocumentIdPage() {
-  return <div>DocumentId</div>;
+"use client";
+
+import { useQueries, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  // comes from the folder name [doucmentId] -> a variable
+  // it is one way to access it
+  // or we can use useParams();
+  // two ways to access it
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+export default function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:mg-max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 }
